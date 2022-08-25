587b616b2a29640a8d1d20695134989ba0a76c09
Guillermo Suarez Reinoso <81838049+guillesr94@users.noreply.github.com>
2022-08-25 10:39:23 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+    $("#button").click(function(){
+        alert("hiciste click en el boton")
+    })
+
+    $("#jquery").click(function(){
+        console.log("Hola estoy utilizando jQuery")
+    })
+
+
+})
